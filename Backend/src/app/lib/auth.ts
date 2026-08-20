3f9dd180198202bf008b9fd2c063e0e3f58c7a63
Soyab-Aktar <iamsoyabcode23@gmail.com>
2026-08-20 21:41:12 +0530

    feat(db): add role, status, and soft-delete fields to users

--- Backend/src/app/lib/auth.ts
+++ Backend/src/app/lib/auth.ts
@@ -2,14 +2,41 @@ import { betterAuth } from "better-auth";
 import { prismaAdapter } from "better-auth/adapters/prisma";
 import { prisma } from "./prisma";
 import { envVars } from "../config/env";
+import { Role, UserStatus } from "../../generated/prisma/enums";
 
 export const auth = betterAuth({
+  secret: envVars.BETTER_AUTH_SECRET,
+  baseURL: envVars.BETTER_AUTH_URL,
   database: prismaAdapter(prisma, {
     provider: "postgresql",
   }),
-  secret: envVars.BETTER_AUTH_SECRET,
-  baseURL: envVars.BETTER_AUTH_URL,
   emailAndPassword: {
     enabled: true,
   },
+
+  user: {
+    additionalFields: {
+      role: {
+        type: "string",
+        required: true,
+        defaultValue: Role.USER
+      },
+      status: {
+        type: "string",
+        required: true,
+        defaultValue: UserStatus.ACTIVE
+      },
+      isDeleted: {
+        type: "boolean",
+        required: true,
+        defaultValue: false
+      },
+      deletedAt: {
+        type: "date",
+        required: false,
+        defaultValue: null
+      }
+    }
+  }
+
 });
